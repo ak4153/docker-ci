e0fee3fdae051dc87cbd1b816e61917fab25afd7
Alex K <64101220+ak4153@users.noreply.github.com>
2024-01-25 10:17:13 +0200

    fix yml

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ redis = Redis(host=redis_host, port=6379)
 @app.route('/')
 def hello_world():
     redis.incr('hits',amount=1)
-    return f'<html><head><title>Python Web Page</title></head><body><h1>hits:{redis.get("hits")}</h1></body></html>'
+    return '<html><body><h1>Hello, World!</h1></body></html>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
